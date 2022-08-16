ec39c2fb86f37418930b3ae742f76de9e59fef4d
YunYeoJin <duwls0349@naver.com>
2022-08-16 09:44:17 +0900

    Initial Commit

---
+++ SeSAC2_Week7_Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2_Week7_Diary
+//
+//  Created by 윤여진 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
